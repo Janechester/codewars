5f8d9febe1d0c15293fcdb6cc16796febc098ab5
Jane Chester <jane_chester@hotmail.com>
2017-11-25 18:27:13 +0100

    Update Codewars.js

--- Codewars.js
+++ Codewars.js
@@ -1,4 +1,5 @@
 Codewars.js
+test
 
 Descending Order (7 kyu)
 Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
